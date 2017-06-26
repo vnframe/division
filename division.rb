88d71b681dd047cbe3e5181b19e537c88f235af4
vnframe <vnframe95@icloud.com>
2017-06-26 11:48:07 -0400

    partial division

---
+++ division.rb
@@ -0,0 +1,7 @@
+print "Please enter number 1 : "
+#input in console
+val1 = gets;
+print "Please enter number 2 : "
+val2 = gets;
+#change val to integer (i)
+print "Answer : " , (val1.to_i / val2.to_i), "\n"
